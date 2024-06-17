d0ac75c3441187121f76b07db737b60eef76da8a
Klaudiusz Marszałek <klaudiusz.w.m@gmail.com>
2024-06-17 11:36:51 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col justify-center items-center min-h-screen">
+      <div className='text-center'>
+        <h1 className='text-3xl'>My App</h1>
+        <p>React, Typescript, Taiwlind</p></div>
     </div>
   );
 }
